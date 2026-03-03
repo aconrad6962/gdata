5b76fc82456776f1879b29c116b6a1c3605c33e0
aconrad6962 <53485665+aconrad6962@users.noreply.github.com>
2026-03-03 12:41:05 -1000

    Add files via upload
    More generic version from Jacob.  Plan to extend command line arguments (i.e., make token optional for public access cases) next time.

--- gdata.py
+++ gdata.py
@@ -7,15 +7,23 @@ import requests
 import shutil
 from datetime import datetime
 from pathlib import Path
+from sys import argv
 
-def glog( log_file, string ):
+script, token_path = argv
+program_id = "AZ-2024B-018"
+obs_date = "250116"
+start_file = 3000
+end_file = 5000
+
+
+def glog(log_file, string):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_file.write(timestamp + ': ' + string )
+    log_file.write(timestamp + ": " + string)
     log_file.flush()
 
-print('gdata does not write to the terminal unless there is an error.')
-print(
-   'To monitor status, type "tail -f gdata.log" in another terminal session.')
+
+print("gdata does not write to the terminal unless there is an error.")
+print('To monitor status, type "tail -f gdata.log" in another terminal session.')
 
 #
 # Construct the service object
@@ -27,50 +35,70 @@ service = pyvo.dal.TAPService(access_url)
 # Set up logging
 #
 log_file = open("gdata.log", "a", buffering=1)
-glog(log_file,'------------- The gdata program was restarted ------------\n')
-glog( log_file, "gdata version 2.0.  November 26, 2025\n" )
+glog(log_file, "------------- The gdata program was restarted ------------\n")
+glog(log_file, "gdata version 2.0.  November 26, 2025\n")
 
 #
 # Form the query
 #
-query = """SELECT lbt.file_name AS file_name FROM lbt.lbt lbt WHERE lbt.program = 'UM-2025B-002' AND lbt.file_name LIKE '%lm\_%'"""
+
+query = f"""SELECT lbt.file_name AS file_name FROM lbt.lbt lbt WHERE lbt.program = '{program_id}' AND lbt.file_name LIKE '%lm\_%'"""
 
 #
 # Token is token downloaded from the LBTO archive
 #
-token = open('token.txt').read()
-headers = {'Authorization': f'Bearer {token}'}
+token = open(token_path).read().strip()
+headers = {"Authorization": f"Bearer {token}"}
 
 #
 # Issue the query
 #
-result = service.run_async( query )
+result = service.run_async(query)
 
-current_dir = Path.cwd()       # current directory
+current_dir = Path.cwd()  # current directory
 #
 # For each file, check if already retrieved in a previous run.
 # If so, continue, else, retrieve it.
 #
-glog( log_file, "Total files found = " + str(len(result)) + "\n" )
+glog(log_file, "Total files found = " + str(len(result)) + "\n")
 for row in result:
-  filename = row["file_name"]
-  file_path = current_dir / filename
-  if file_path.exists():
-    glog( log_file, 'skipping ' + filename + '\n' )
-    continue
-  else:
-    glog( log_file, filename + ' retrieval started\n' )
+    filename = row["file_name"]
+    file_path = current_dir / filename
 
-    # Form the URL
-    url = 'https://archive.lbto.org/files/lbt/' + filename
-
-    # Retrieve the file
-    r = requests.get(url, headers=headers, stream=True)
-    try: r.raise_for_status()
-    except Exception as e:
-        glog( log_file, "failed {url}: {e!r}" )
+    date = filename.split("_")[1]
+    if date != obs_date:
+        glog(log_file, "skipping " + filename + " because it's from the wrong day\n")
         continue
-    with open(filename, 'wb+') as fd:
-        shutil.copyfileobj(r.raw, fd)
 
-    glog( log_file, filename + ' retrieval complete\n' )
+    file_number = int(filename.split("_")[-1].split(".")[0])
+
+    if file_number < start_file or file_number > end_file + 1:
+        glog(
+            log_file, "skipping " + filename + " because file number is out of range\n"
+        )
+        continue
+
+    if file_path.exists():
+        glog(log_file, "skipping " + filename + " because it already exists\n")
+        continue
+    else:
+        glog(log_file, filename + " retrieval started\n")
+
+        # Form the URL
+        url = (
+            "https://archive.lbto.org/files/lbt"
+            + filename
+        )
+
+        # Retrieve the file
+        r = requests.get(url, headers=headers, stream=True)
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            glog(log_file, "failed {url}: {e!r}")
+            continue
+        with open(filename, "wb+") as fd:
+            shutil.copyfileobj(r.raw, fd)
+
+        glog(log_file, filename + " retrieval complete\n")
+
